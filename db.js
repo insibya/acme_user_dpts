e4679a6b04db010cc44c277a349ecd2369c0935d
Nathan Roy <nathanmroy@yahoo.com>
2019-09-21 12:11:42 -0700

    updated db

--- db.js
+++ db.js
@@ -1,24 +1,52 @@
 const Sequelize = require('sequelize');
-const { STRING } = Sequelize;
+const { STRING, UUID, UUIDV4 } = Sequelize;
 
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/acme_user_dpts');
 
 const User = conn.define('user', {
-  name: String
+	id: {
+		type: UUID,
+		primaryKey: true,
+		defaultValue: UUIDV4
+	},
+	name: {
+		type: String,
+		allowNull: false,
+		unique: true
+	},
+	deptId: UUID
 });
 
-const syncAndSeed = async()=> {
-  const users = [
-    { name: 'chris'},
-    { name: 'bob'},
-    { name: 'ruby'},
-  ];
-  const [ chris, bob, ruby ] = await Promise.all(users.map(user => User.create(user)));
-  return {
-    users: {
-      chris,
-      bob,
-      ruby
-    }
-  };
+const Dept = conn.define('dept', {
+	id: {
+		type: UUID,
+		primaryKey: true,
+		defaultValue: UUIDV4
+	},
+	name: {
+		type: String,
+		allowNull: false,
+		unique: true
+	}
+});
+
+const syncAndSeed = async () => {
+	const users = [ { name: 'chris' }, { name: 'bob' }, { name: 'ruby' } ];
+	const [ chris, bob, ruby ] = await Promise.all(users.map((user) => User.create(user)));
+
+	const depts = [ { name: 'engineering' }, { name: 'marketing' }, { name: 'espionage' } ];
+	const [ engineering, marketing, espionage ] = await Promise.all(depts.map((dept) => Dept.create(dept)));
+
+	return {
+		users: {
+			chris,
+			bob,
+			ruby
+		},
+		depts: {
+			engineering,
+			marketing,
+			espionage
+		}
+	};
 };
--- spec.js
+++ spec.js
@@ -1,14 +1,19 @@
 const { expect } = require('chai');
 const db = require('./db');
 
-describe('Acme_users_dpts', ()=> {
-  let seed;
-  beforeEach(async()=> seed = await db.syncAndSeed());
-  describe('Data Layer', ()=> {
-    it('Chris, Bob, Ruby are users', ()=> {
-      expect(seed.users.chris.name).to.equal('chris');
-      expect(seed.users.bob.name).to.equal('bob');
-      expect(seed.users.ruby.name).to.equal('ruby');
-    });
-  });
+describe('Acme_users_dpts', () => {
+	let seed;
+	beforeEach(async () => (seed = await db.syncAndSeed()));
+	describe('Data Layer', () => {
+		it('Chris, Bob, Ruby are users', () => {
+			expect(seed.users.chris.name).to.equal('chris');
+			expect(seed.users.bob.name).to.equal('bob');
+			expect(seed.users.ruby.name).to.equal('ruby');
+		});
+		it('Engineering, marketing and espionage', () => {
+			expect(seed.depts.engineering.name).to.equal('engineering');
+			expect(seed.depts.marketing.name).to.equal('marketing');
+			expect(seed.depts.espionage.name).to.equal('espionage');
+		});
+	});
 });
